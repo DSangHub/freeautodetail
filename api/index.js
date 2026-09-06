a16e0ebb2491c40f9ee415a4a0d0858dd378ecb9
DSangHub <dsangco08@gmail.com>
2026-09-06 10:41:49 -0700

    Fix Vercel API routing (#5)
    * Add Vercel API function entry point
    
    * Route API requests to the Express function

---
+++ api/index.js
@@ -0,0 +1,3 @@
+// Vercel Function entry point. All /api/* requests are rewritten here and
+// handled by the Express application defined at the repository root.
+module.exports = require("../server");
